064bbd6f18dc0e9ee294c17274e8bf833ee20a4d
ilyamore88 <themorozik@gmail.com>
2018-11-02 17:33:35 +0300

    done task 14

---
+++ lab7/src/Chair.java
@@ -0,0 +1,16 @@
+public class Chair extends Furniture {
+    private String color;
+
+    public Chair(String material, double age, String color) {
+        super(material, age);
+        this.color = color;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("from Furniture: ");
+        super.print();
+        System.out.println("Chair: ");
+        System.out.println("material: " + this.material + "; age: " + this.age + "; color: " + this.color);
+    }
+}
